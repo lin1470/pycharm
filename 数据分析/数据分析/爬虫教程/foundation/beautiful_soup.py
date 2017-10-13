6b99946c75545a3e731dc506593bd02a829772eb
Bruce <243381816@qq.com>
2017-10-13 15:36:05 +0800

    pycharm

--- 数据分析/数据分析/爬虫教程/foundation/beautiful_soup.py
+++ 数据分析/数据分析/爬虫教程/foundation/beautiful_soup.py
@@ -48,10 +48,55 @@ soup = BeautifulSoup(html,"lxml")
 # print type(soup.name)
 
 # 4.Comment omment 对象是一个特殊类型的 NavigableString 对象，其实输出的内容仍然不包括注释符号，但是如果不好好处理它，可能会对我们的文本处理造成意想不到的麻烦。
-print soup.a
-print soup.a.string
-print type(soup.a.string)
-# 另外我们打印输出下它的类型，发现它是一个 Comment 类型，所以，我们在使用前最好做一下判断，判断代码如下
+# print soup.a
+# print soup.a.string
+# print type(soup.a.string)
+# # 另外我们打印输出下它的类型，发现它是一个 Comment 类型，所以，我们在使用前最好做一下判断，判断代码如下
+#
+# if type(soup.a.string)== bs4.element.Comment:
+#     print soup.a.string
 
-if type(soup.a.string)== bs4.element.Comment:
-    print soup.a.string
+# .contents
+#
+# tag 的 .content 属性可以将tag的子节点以列表的方式输出
+# print soup.html.contents[2].prettify()
+# print len(soup.head.contents)
+
+# .children
+# 它返回的不是一个 list，不过我们可以通过遍历获取所有子节点。
+# 我们打印输出 .children 看一下，可以发现它是一个 list 生成器对象
+# print soup.head.children
+# # print soup.head.title.string.prettify()
+# for child in soup.html.children:
+#     print child.name  # what happen?
+#
+
+
+# for child in soup.descendants:
+#     print child.name
+# print soup.name
+# print soup.p.parent.name
+# content = soup.head.title.string
+# for parent in content.parents:
+#     print parent.name
+# print soup.p.next_sibling.next_sibling.prettify()
+# print soup.p.pre_sibling
+# print "hh"
+# for sibling in soup.a.next_siblings:
+#     print repr(sibling)
+
+# （1）find_all( name , attrs , recursive , text , **kwargs )
+# find_all() 方法搜索当前tag的所有tag子节点,并判断是否符合过滤器的条件
+# 1）name 参数
+# name 参数可以查找所有名字为 name 的tag,字符串对象会被自动忽略掉
+# print soup.find_all('b')
+# print soup.find_all('a')
+# import re
+# for tag in soup.find_all(re.compile('body')):
+#     print tag.name
+# print soup.find_all(['a','b'])
+# gen = [x for x in range(10)]  # very easy to create a list
+# print gen
+def has_class_but_no_id(tag):
+    return tag.has_attr('class') and not tag.has_attr('id')
+print soup.find_all(has_class_but_no_id)
--- 数据分析/数据分析/爬虫教程/regex_1.py
+++
@@ -1,8 +0,0 @@
-# coding:UTF-8
-#废话
-import re
-
-pattern = re.compile(r'\d+')
-print pattern.findall('one1two2three3four4')  # return a result with a list
-for m in re.finditer(pattern,'one1two2three3four4'):
-    print m.group(),  # use a common to avoid to switch a new line
